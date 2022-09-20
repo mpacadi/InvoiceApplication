2763c2a1577b8b37b8eff2fd67b30ae9484897b4
Marko Pacadi <marko.pacadi@fer.hr>
2022-09-20 23:08:49 +0200

    Add initial migrations

---
+++ InvoiceApplication/Migrations/202209202100280_Initial.cs
@@ -0,0 +1,168 @@
+﻿namespace InvoiceApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.InvoiceProduct",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        SellCount = c.Int(nullable: false),
+                        PriceTaxFree = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        TotalPriceTaxFree = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ProductId = c.Int(nullable: false),
+                        InvoiceId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Invoice", t => t.InvoiceId, cascadeDelete: true)
+                .ForeignKey("dbo.Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.InvoiceId);
+            
+            CreateTable(
+                "dbo.Invoice",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        InvoiceCreated = c.DateTime(nullable: false),
+                        InvoicePayday = c.DateTime(nullable: false),
+                        TotalTaxFree = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        TotalTax = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CustomerName = c.String(),
+                        InvoiceTaxId = c.Int(nullable: false),
+                        InvoiceCreator_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.InvoiceCreator_Id)
+                .ForeignKey("dbo.InvoiceTax", t => t.InvoiceTaxId, cascadeDelete: true)
+                .Index(t => t.InvoiceTaxId)
+                .Index(t => t.InvoiceCreator_Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.InvoiceTax",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TaxName = c.String(),
+                        Tax = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.InvoiceProduct", "ProductId", "dbo.Product");
+            DropForeignKey("dbo.Invoice", "InvoiceTaxId", "dbo.InvoiceTax");
+            DropForeignKey("dbo.InvoiceProduct", "InvoiceId", "dbo.Invoice");
+            DropForeignKey("dbo.Invoice", "InvoiceCreator_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Invoice", new[] { "InvoiceCreator_Id" });
+            DropIndex("dbo.Invoice", new[] { "InvoiceTaxId" });
+            DropIndex("dbo.InvoiceProduct", new[] { "InvoiceId" });
+            DropIndex("dbo.InvoiceProduct", new[] { "ProductId" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Product");
+            DropTable("dbo.InvoiceTax");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Invoice");
+            DropTable("dbo.InvoiceProduct");
+        }
+    }
+}
---
+++ InvoiceApplication/Migrations/Configuration.cs
@@ -0,0 +1,39 @@
+﻿namespace InvoiceApplication.Migrations
+{
+    using InvoiceApplication.Models.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<InvoiceApplication.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(InvoiceApplication.Models.ApplicationDbContext context)
+        {
+            var products = new List<Product>
+            {
+                new Product{Id = 1, Name="Pencile", Description="A wood pencile", Price=10.00m},
+                new Product{Id = 2, Name="Pen", Description="A mechanical pencile", Price=15.00m},
+                new Product{Id = 3, Name="Eraser", Description="A eraser for pencile", Price=5.00m}
+            };
+
+            products.ForEach(s => context.Products.AddOrUpdate(p => p.Id, s));
+            context.SaveChanges();
+
+            var taxes = new List<InvoiceTax>
+            {
+                new InvoiceTax{Id=1, TaxName="HR", Tax=18m},
+                new InvoiceTax{Id=2, TaxName="BIH", Tax=20m}
+            };
+
+            taxes.ForEach(s => context.InvoiceTaxes.AddOrUpdate(p => p.Id, s));
+            context.SaveChanges();
+        }
+    }
+}
