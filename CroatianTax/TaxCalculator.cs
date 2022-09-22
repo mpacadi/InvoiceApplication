b88a2cc657343447b7309a00eb157c534bd0bbdc
Marko Pacadi <marko.pacadi@fer.hr>
2022-09-22 21:26:40 +0200

    Add mef project for croatian tax

---
+++ CroatianTax/TaxCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvoiceApplication.Interfaces;
+
+namespace CroatianTax
+{
+    public class TaxCalculator : ITaxCalculator
+    {
+        [Export("HR", typeof(ITaxCalculator))]
+        public decimal CalculateTax(decimal price)
+        {
+            return price * 1.25m;
+        }
+    }
+}
---
+++ InvoiceApplication/Interfaces/ITaxCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApplication.Interfaces
+{
+    public interface ITaxCalculator
+    {
+        decimal CalculateTax(decimal price);
+    }
+}
