2d4e229a52ed7160bcaa18cc34ec17e54c5847d3
Marko Pacadi <marko.pacadi@fer.hr>
2022-09-22 22:28:55 +0200

    Add mef project for bosnian tax

---
+++ BosnianTax/TaxCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvoiceApplication.Interfaces;
+
+namespace BosnianTax
+{
+    [Export("BIH", typeof(ITaxCalculator))]
+    public class TaxCalculator : ITaxCalculator
+    {
+        public decimal CalculateTax(decimal price)
+        {
+            return price * 1.17m;
+        }
+    }
+}
